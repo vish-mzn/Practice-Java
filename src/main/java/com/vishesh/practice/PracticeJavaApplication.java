11d3fc70e952072123d0c318c700d45dfad94993
Vishesh Pahuja <visheshpahuja@Visheshs-MacBook-Air.local>
2024-03-17 19:10:49 +0800

    first commit

---
+++ src/main/java/com/vishesh/practice/PracticeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.vishesh.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeJavaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vishesh/practice/pojo/Country.java
@@ -0,0 +1,25 @@
+package com.vishesh.practice.pojo;
+
+public class Country {
+	
+	private String name;
+	
+	private String region;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+}
